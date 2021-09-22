59386254f8a53d9c3ea9860cd029b2946f684538
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-22 11:19:46 +0530

    Added: code to perform math operations using lambda expression

--- src/com/lambda/Main.java
+++ src/com/lambda/Main.java
@@ -1,10 +1,21 @@
 package com.lambda;
-
+/**
+ * Main class demonstrates use of lambda expression
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		
+
+		int a = 20;
+		int b = 10;
 		System.out.println("Welcome to lambda expression program");
+
+		IMathFunction add = (x, y) -> x + y;
+		IMathFunction mul = (x, y) -> x * y;
+		IMathFunction div = (x, y) -> x / y;
+		System.out.println("Addition : " + add.calculate(a, b));
+		System.out.println("Multiplication : " + mul.calculate(a, b));
+		System.out.println("Division : " + div.calculate(a, b));
 	}
 
 }
