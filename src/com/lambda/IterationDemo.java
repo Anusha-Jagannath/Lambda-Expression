6fa7d54797f325d285788db15b745a9ddb8721b7
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-22 13:13:27 +0530

    Added: code using forEach method

--- src/com/lambda/IterationDemo.java
+++ src/com/lambda/IterationDemo.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * IterationDemo class contains static method printNumbers to print list
@@ -18,5 +19,30 @@ public class IterationDemo {
 		while (iterator.hasNext()) {
 			System.out.print(iterator.next() + " ");
 		}
+
+		// method 2 using Consumer interface
+		MyConsumer<Integer> consumer = new MyConsumer<>();
+		integers.forEach(consumer);
+
+		// method 3 traversing with anonymous consumer interface
+		integers.forEach(new Consumer<Integer>() {
+
+			@Override
+			public void accept(Integer t) {
+				System.out.println("Method 3: forEach anonymous class value : " + t);
+			}
+		});
+
+		// method4: explicit lambda function
+		Consumer<Integer> consumer1 = t -> {
+			System.out.println("Method 4: forEach Lambda impl value " + t);
+		};
+		integers.forEach(consumer1);
+
+		// method 5:implicit lambda function
+		integers.forEach(t -> {
+			System.out.println("Method5: forEach lambda imple value " + t);
+		});
+
 	}
 }
